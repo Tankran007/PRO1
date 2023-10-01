af1cdbbbb7fc978b5e68402ac607bebf84fc2433
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-10-01 17:30:45 +0700

    update

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/msn", async (req, res) => {
+  try {
+      const response = await axios.get(base_url + '/test');
+      res.render("music", { music: response.data });
+  } catch (err) {
+      console.error(err);
+      res.status(500).send('Error');
+  }
+});
+
 app.get("/formatview", async (req, res) => {
   try {
       const response = await axios.get(base_url + '/yuo');
